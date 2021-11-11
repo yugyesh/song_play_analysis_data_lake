bffa2d0b5ed3d965795a8792b17c47ffae3ccae5
yugyesh <uges.kk@gmail.com>
2021-11-11 22:10:06 +0900

    env: Install pyspark

---
+++ etl.py
@@ -0,0 +1,6 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
