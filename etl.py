655e40598a68f33cd8281eb68c864b78da01ae52
yugyesh <uges.kk@gmail.com>
2021-11-16 13:36:54 +0900

    test: Add ipynb file for testing

--- etl.py
+++ etl.py
@@ -4,3 +4,25 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+
+config = configparser.ConfigParser()
+config.read("dl.cfg")
+
+os.environ["AWS_ACCESS_KEY_ID"] = config["AWS"]["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = config["AWS"]["AWS_SECRET_ACCESS_KEY"]
+
+
+def create_spark_session():
+    spark = SparkSession.builder.config(
+        "spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0"
+    ).getOrCreate()
+    return spark
+
+
+def main():
+    spark = create_spark_session()
+
+
+if __name__ == "__main__":
+    main()
